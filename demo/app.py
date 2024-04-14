6242c4bb55c945756dac7dd14f12c097f6060ed6
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2024-04-14 19:15:42 +0200

    v1.2.2: Demo app UX updates

--- demo/app.py
+++ demo/app.py
@@ -2,6 +2,7 @@ import contextlib
 
 import pandas as pd
 import streamlit as st
+from st_social_media_links import SocialMediaIcons
 
 from st_supabase_connection import SupabaseConnection, __version__
 
@@ -60,8 +61,44 @@ with st.sidebar:
         st.cache_resource.clear()
         st.success("Cache cleared")
 
-    st.components.v1.html(sidebar_html, height=600)
+    st.components.v1.html(sidebar_html, height=247)
 
+    st.html(
+        """
+        <div style="text-align:center; font-size:14px; color:lightgrey">
+            <hr style="margin-bottom: 6%; margin-top: 0%;">
+            Share the ❤️ on social media
+        </div>"""
+    )
+
+    social_media_links = [
+        "https://www.facebook.com/sharer/sharer.php?kid_directed_site=0&sdk=joey&u=https%3A%2F%2Fst-supabase-connection.streamlit.app%2F&display=popup&ref=plugin&src=share_button",
+        "https://www.linkedin.com/sharing/share-offsite/?url=https%3A%2F%2Fst-supabase-connection.streamlit.app%2F",
+        "https://twitter.com/intent/tweet?original_referer=http%3A%2F%2Flocalhost%3A8501%2F&ref_src=twsrc%5Etfw%7Ctwcamp%5Ebuttonembed%7Ctwterm%5Eshare%7Ctwgr%5E&text=Check%20out%20this%20Streamlit%20Supabase%20connection%20demo%20app%20%F0%9F%8E%88&url=https%3A%2F%2Fst-supabase-connection.streamlit.app%2F",
+    ]
+
+    social_media_icons = SocialMediaIcons(
+        social_media_links, colors=["lightgray"] * len(social_media_links)
+    )
+
+    social_media_icons.render(sidebar=True)
+
+    st.html(
+        """
+        <div style="text-align:center; font-size:12px; color:lightgrey">
+            <hr style="margin-bottom: 6%; margin-top: 6%;">
+            <a rel="license" href="https://creativecommons.org/licenses/by-nc-sa/4.0/">
+                <img alt="Creative Commons License" style="border-width:0"
+                    src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" />
+            </a><br><br>
+            This work is licensed under a <b>Creative Commons
+                Attribution-NonCommercial-ShareAlike 4.0 International License</b>.<br>
+            You can modify and build upon this work non-commercially. All derivatives should be
+            credited to Siddhant Sadangi and
+            be licenced under the same terms.
+        </div>
+    """
+    )
 # ---------- MAIN PAGE ----------
 st.header("🔌Supabase Connection for Streamlit", divider="violet")
 
@@ -1054,3 +1091,8 @@ st_supabase.auth.verify_otp(dict(type="magiclink", email=email, token=token))
                     st.error("No logged-in user. Log in or sign up first.", icon="❌")
                 else:
                     st.error(e, icon="❌")
+
+st.success(
+    "[Star the repo](https://github.com/SiddhantSadangi/st_supabase_connection) to show your :heart:",
+    icon="⭐",
+)
