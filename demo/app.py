522581f66c246e01f9f75ba920489bc7c4a29c1b
Siddhant Sadangi <siddhant.sadangi@gmail.com>
2025-07-25 21:39:24 +0200

    v2.1.0 (chore, app): Adjust sidebar content

--- demo/app.py
+++ demo/app.py
@@ -65,7 +65,7 @@ with st.sidebar:
         st.cache_resource.clear()
         st.success("Cache cleared")
 
-    st_html(sidebar_html, height=350)
+    st_html(sidebar_html, height=340)
 
     st.html(
         """
